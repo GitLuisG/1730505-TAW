6af4332c3ee212ebf30e5607fcdaece934ed8fa9
Luis Gerardo Perales Torres <1730505@upv.edu.mx>
2020-09-14 19:59:01 -0500

    avance modificacion

--- Practica 2/controllers/controller.php
+++ Practica 2/controllers/controller.php
@@ -30,8 +30,28 @@ class MvcController{
           $datosController, "usuarios"
       );
       //recibir la respuesta del modelo para saber que sucedio (succes o error)
-      
+      if(respuesta == "Success"){
+        header("location: index.php?action=ok");
+      }else{
+        header("location: index.php");
+      }
 
+    }
+
+    public function ingresoUsuarioController(){
+        if(isset($_POST["usuarioIngreso"])){
+            $datosController= array("usuario"=>$_POST["usuarioIngreso"],
+                                    "password"=>$_POST["passwordIngreso"]);
+            $respuesta = Datos::ingresoUsuarioModel($datosController, "usuario");
+            if($respuesta["usuario"]==$_POST["usuarioIngreso"] && $respuesta["password"]==$_POST["passwordIngreso"]){
+                session_start();
+                $_SESSION["validar"] = true;
+                header("location: index.php?action=usuarios");
+            }
+            else{
+                header("location: index.php?action=fallo");
+            }
+        }
     }
 
 }
--- Practica 2/models/crud.php
+++ Practica 2/models/crud.php
@@ -3,6 +3,16 @@ require_once "conexion.php";
 
 class Datos extends Conexion {
     public function reegistroUsuarioModel($datosModel, $tabla){
-        
+        $stmt = Conexion::conectar()->prepare("INSERT INTO $tabla(usuario, contrasena, email) VALUES (:usuario, :password, :email)");
+        $stmt->bindParam(":usuario", $datosModel["usuario"], PDO::PARAM_STR);
+        $stmt->bindParam(":password", $datosModel["contrasena"], PDO::PARAM_STR);
+        $stmt->bindParam(":email", $datosModel["email"], PDO::PARAM_STR);
+        if($stmt->execute()){
+            return "success";
+        }
+        else{
+            return "error";
+        }
+        $stmt->close();
     }
 }
